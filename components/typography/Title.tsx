638a8550da7bbd65cbd9a2270e709f1101d5290f
Anthony Riley <anthonyriley@stanford.edu>
2022-07-30 20:15:48 -0700

    Fixed some dead links and date issues

--- components/typography/Title.tsx
+++ components/typography/Title.tsx
@@ -3,9 +3,11 @@ import { ReactNode } from "react";
 export default function Title({
   children,
   color = "gold",
+  id,
 }: {
   children: string | ReactNode;
   color?: "gold" | "blue" | "purple";
+  id?: string;
 }) {
   const gradients = {
     gold: "bg-gradient-to-b from-orange-400 to-yellow-500",
@@ -13,7 +15,7 @@ export default function Title({
     purple: "bg-gradient-to-b from-purple-600 to-purple-500",
   };
   return (
-    <div className="flex flex-row items-center my-16">
+    <div className="flex flex-row items-center my-16" id={id}>
       <div className={`${gradients[color]} w-2 h-10 mr-4`} />
       <h2 className="text-3xl md:text-4xl font-bold text-center md:text-left">
         {children}
